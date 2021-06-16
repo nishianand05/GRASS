c806f60873c37d1b280b67dfe2d74937ff0137c5
nishianand05 <nishianand.05@gmail.com>
2021-06-16 19:39:06 +0530

    Commit initial scanner files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.render("scanner");
+});
+
+app.post("/scan", (req, res) => {
+    const qrcode = req.body.qrcode;
+    res.render("scan", {qrcode: qrcode});
+});
+
+app.listen(port, () => console.log("Server at " + port))
